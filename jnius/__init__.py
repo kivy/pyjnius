e3a29658106140d3dec5c135ea5bf36938514aec
Mathieu Virbel <mat@kivy.org>
2012-08-20 18:48:22 +0200

    bump to 1.1-dev

--- jnius/__init__.py
+++ jnius/__init__.py
@@ -7,7 +7,7 @@ Accessing Java classes from Python.
 All the documentation is available at: http://pyjnius.readthedocs.org
 '''
 
-__version__ = '1.0'
+__version__ = '1.1-dev'
 
 from .jnius import *
 from .reflect import *
