f9c63fd186fe78089f5610085274efe14ea39eb6
Mathieu Virbel <mat@kivy.org>
2012-08-20 18:47:44 +0200

    set version to 1.0

--- jnius/__init__.py
+++ jnius/__init__.py
@@ -1,2 +1,13 @@
+'''
+Pyjnius
+=======
+
+Accessing Java classes from Python.
+
+All the documentation is available at: http://pyjnius.readthedocs.org
+'''
+
+__version__ = '1.0'
+
 from .jnius import *
 from .reflect import *
