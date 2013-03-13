a1c18107d32dbeac47e23617cfcee1e051f85894
Mathieu Virbel <mat@kivy.org>
2013-03-13 19:12:53 +0100

    add DEBUG boolean in the java class

--- jnius/src/org/jnius/NativeInvocationHandler.java
+++ jnius/src/org/jnius/NativeInvocationHandler.java
@@ -3,37 +3,35 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
 public class NativeInvocationHandler implements InvocationHandler {
+    static boolean DEBUG = false;
+    private long ptr;
+
     public NativeInvocationHandler(long ptr) {
         this.ptr = ptr;
     }
 
     public Object invoke(Object proxy, Method method, Object[] args) {
-        /**
-        if ( method.getName() == "toString" ) {
-            System.out.println("+ java:invoke toString.");
-            return "<proxy>";
+        if ( DEBUG ) {
+            System.out.print("+ java:invoke(<proxy>, ");
+            // don't call it, or recursive lookup/proxy will go!
+            //System.out.print(proxy);
+            //System.out.print(", ");
+            System.out.print(method);
+            System.out.print(", ");
+            System.out.print(args);
+            System.out.println(")");
+            System.out.flush();
         }
-        /**/
-        System.out.print("+ java:invoke(<proxy>, ");
-        // don't call it, or recursive lookup/proxy will go!
-        //System.out.print(proxy);
-        //System.out.print(", ");
-        System.out.print(method);
-        System.out.print(", ");
-        System.out.print(args);
-        System.out.println(")");
-        System.out.flush();
 
-        //System.out.print("+ java:call native invoke0() >>>");
         Object ret = invoke0(proxy, method, args);
-        //System.out.println("+ java:call native invoke0() <<<");
 
-        System.out.print("+ java:invoke returned: ");
-        System.out.println(ret);
+        if ( DEBUG ) {
+            System.out.print("+ java:invoke returned: ");
+            System.out.println(ret);
+        }
+
         return ret;
     }
 
     native Object invoke0(Object proxy, Method method, Object[] args);
-
-    private long ptr;
 }
