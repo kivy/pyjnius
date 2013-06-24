68165ed04b04ae214f5b4ba0eada3a4e56d03e8f
Mathieu Virbel <mat@kivy.org>
2013-06-24 09:24:37 +0200

    add tests for testing Python string -> [B, using ByteBuffer.wrap(byte[])

---
+++ tests/test_bytearray.py
@@ -0,0 +1,9 @@
+import unittest
+from jnius import autoclass
+
+class StringArgumentForByteArrayTest(unittest.TestCase):
+
+    def test_string_arg_for_byte_array(self):
+        # the ByteBuffer.wrap() accept only byte[].
+        ByteBuffer = autoclass('java.nio.ByteBuffer')
+        self.assertIsNotNone(ByteBuffer.wrap('hello world'))
