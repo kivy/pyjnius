72e7f030b4e30cf3ee0eadd6b37a38e1b0c0316a
Barry Wark <barrywark@gmail.com>
2013-07-10 14:17:06 -0400

    Version 1.2

--- jnius/__init__.py
+++ jnius/__init__.py
@@ -7,7 +7,7 @@ Accessing Java classes from Python.
 All the documentation is available at: http://pyjnius.readthedocs.org
 '''
 
-__version__ = '1.1-dev'
+__version__ = '1.2'
 
 from .jnius import *
 from .reflect import *
