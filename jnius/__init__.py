5f6aa738b2b7ae496ec43bfadbf9933ee640bb7b
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2019-02-04 10:58:50 +0100

    Bump to 1.2.1.dev0

--- jnius/__init__.py
+++ jnius/__init__.py
@@ -7,7 +7,7 @@ Accessing Java classes from Python.
 All the documentation is available at: http://pyjnius.readthedocs.org
 '''
 
-__version__ = '1.2.0'
+__version__ = '1.2.1.dev0'
 
 from .jnius import *  # noqa
 from .reflect import *  # noqa
