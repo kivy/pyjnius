13028c18e9fbdccbfe553b0700453901196fa6dd
Gabriel Pettier <gabriel.pettier@gmail.com>
2019-11-24 14:53:00 +0100

    prepare work for automated release on pypi/github

--- .github/actions/scripts/release.sh
+++ .github/actions/scripts/release.sh
@@ -1,12 +1,41 @@
 #!/usr/bin/env bash
 set -ex
 
-if [[ "refs/tags/$(git tag | tail -n 1)" = $GITHUB_REF ]]; then
-    pip install -U setuptools wheel twine
-    python -m twine check dist/*
-  if [[ $GITHUB_REF =~ -test$ ]]; then
-    python -m twine upload -u kivybot --disable-progress-bar --repository-url https://test.pypi.org/legacy/ dist/*
-  else
-    python -m twine upload -u kivybot --disable-progress-bar dist/*
-  fi
+if [[ ! $GITHUB_REF =~ ^refs/tags/ ]]; then
+   exit 0
 fi
+
+owner=kivy
+repository=pyjnius
+access_token=$GITHUB_TOKEN
+
+tag=${GITHUB_REF#refs/tags/}
+draft="false"
+prerelease="false"
+version_name="$tag"
+message="Release $tag"
+
+python -m twine check dist/*
+
+twine="python -m twine upload -u kivybot --disable-progress-bar"
+
+if [[ $GITHUB_REF =~ -test$ ]]; then
+    twine="$twine --repository-url https://test.pypi.org/legacy/"
+    draft="true"
+    prerelease="true"
+    message="test release $tag"
+fi
+
+API_JSON="{
+    \"tag_name\": \"$tag\",
+    \"name\": \"$version_name\",
+    \"body\": \"$message\n\",
+    \"draft\": $draft,
+    \"prerelease\": $prerelease
+}"
+
+echo $API_JSON
+
+$twine dist/*
+curl --data "$API_JSON"\
+     Https://api.github.com/repos/$owner/$repository/releases?access_token=$access_token
