f328575e486111fd45761603cd0869b1b3b4ff40
Gabriel Pettier <gabriel.pettier@gmail.com>
2019-11-25 01:22:21 +0100

    fix python versions

--- .github/actions/scripts/manylinux_entrypoint.sh
+++ .github/actions/scripts/manylinux_entrypoint.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 target=$1
+echo $target
 
-# yum install -y java-1.7.0-openjdk-devel
-ls /opt/python/
-python=/opt/python/$target/python
+yum install -y java-1.7.0-openjdk-devel
+ls /opt/python/$target/
+python=/opt/python/$target/bin/python
 $python -m pip install -U setuptools wheel cython
 $python setup.py bdist_wheel
