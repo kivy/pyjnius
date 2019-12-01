76e100b0d5a2eec8069778da6e10cedef2ac97d0
Gabriel Pettier <gabriel.pettier@gmail.com>
2019-12-01 02:11:34 +0100

    build all manylinux in one step, no matrix

--- .github/actions/scripts/manylinux_entrypoint.sh
+++ .github/actions/scripts/manylinux_entrypoint.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
-target=$1
-echo $target
-
 yum install -y java-1.7.0-openjdk-devel
 
-ls /opt/python/$target/
-python=/opt/python/$target/bin/python
-$python -m pip install -U setuptools cython
-$python setup.py bdist_wheel
+for target in $(ls /opt/python/); do
+    python=/opt/python/$target/bin/python
+    $python -m pip install -U setuptools cython
+    $python setup.py bdist_wheel
+done
 
 for whl in dist/*.whl; do
     auditwheel repair $whl
