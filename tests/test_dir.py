1ee5d2958706bdce2f818136792cd6d55b5aec20
Young Ryul Bae <amsukdu@gmail.com>
2019-12-15 14:41:51 +0900

    add more comments and typo fix

--- tests/test_dir.py
+++ tests/test_dir.py
@@ -7,24 +7,32 @@ from jnius.reflect import autoclass
 class DirTest(unittest.TestCase):
 
     def test_varargs_dir(self):
+        # >>> from jnius import autoclass
+        # >>> cls = autoclass('java.lang.System')
+        # >>> dir(cls.out.printf)
+        # [(['java/lang/String', 'java/lang/Object...'], 'java/io/PrintStream'),
+        # (['java/util/Locale', 'java/lang/String', 'java/lang/Object...'], 'java/io/PrintStream')]
         cls = autoclass("java.lang.System")
         assert isinstance(dir(cls.out.printf), list)
-        #[(['java/lang/String', 'java/lang/Object...'], 'java/io/PrintStream'),
-        # (['java/util/Locale', 'java/lang/String', 'java/lang/Object...'], 'java/io/PrintStream')]
         for f in dir(cls.out.printf):
             assert isinstance(f, tuple)
 
     def test_array_dir(self):
+        # >>> from jnius import autoclass
+        # >>> cls = autoclass('java.util.List')
+        # >>> dir(cls.toArray)
+        # [([], 'java/lang/Object[]'),
+        # (['java/lang/Object[]'], 'java/lang/Object[]')]
         cls = autoclass("java.util.List")
         assert isinstance(dir(cls.toArray), list)
-        #[([], 'java/lang/Object[]'),
-        # (['java/lang/Object[]'], 'java/lang/Object[]')]
+
         for f in dir(cls.toArray):
             assert isinstance(f, tuple)
 
     def test_dir(self):
-        cls = autoclass("java.lang.String")
-        assert isinstance(dir(cls.valueOf), list)
+        # >>> from jnius import autoclass
+        # >>> cls = autoclass('java.lang.String')
+        # >>> dir(cls.valueOf)
         # [(['boolean'], 'java/lang/String'),
         #  (['char'], 'java/lang/String'),
         #  (['char[]'], 'java/lang/String'),
@@ -34,6 +42,8 @@ class DirTest(unittest.TestCase):
         #  (['int'], 'java/lang/String'),
         #  (['java/lang/Object'], 'java/lang/String'),
         #  (['long'], 'java/lang/String')]
+        cls = autoclass("java.lang.String")
+        assert isinstance(dir(cls.valueOf), list)
         for f in dir(cls.charAt):
             assert isinstance(f, tuple)
 
