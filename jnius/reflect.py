a6713bf87334a13ae02935fdd674b8765a9ae072
Gabriel Pettier <gabriel.pettier@gmail.com>
2020-04-19 00:11:57 +0200

    fixes writting to StaticField through the class

--- jnius/reflect.py
+++ jnius/reflect.py
@@ -360,7 +360,7 @@ def autoclass(clsname, include_protected=False, include_private=False):
                 py_field_name = py_field_name + "_" if py_field_name in classDict else py_field_name
                 print("resolved %s to %s" % (field_name, py_field_name))
                 classDict[py_field_name] = cls(sig, j_name= field_name, j_class_name = owningCls.getName())
-    
+
     classDict['__javaclass__'] = clsname.replace('.', '/')
     return MetaJavaClass.__new__(
         MetaJavaClass,
