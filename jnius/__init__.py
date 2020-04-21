49d01f57053a70764100b1ed0a6fdd11dac5f43d
Gabriel Pettier <gabriel.pettier@gmail.com>
2020-04-22 00:38:26 +0200

    Release 1.3.0
    Full Changelog: https://github.com/kivy/pyjnius/compare/1.2.1...1.3.0
    
    Implemented enhancements:
    - (https://github.com/kivy/pyjnius/pull/483)/(https://github.com/kivy/pyjnius/pull/489) allow passing a `signature` argument to constructors, to force selection of the desired one
    - (https://github.com/kivy/pyjnius/pull/497)/(https://github.com/kivy/pyjnius/pull/506)/(https://github.com/kivy/pyjnius/pull/507) support for more "dunder" methods/protocols on compatible interfaces than just `__len__`, and allow users to provide their own.
    - (https://github.com/kivy/pyjnius/pull/500)(https://github.com/kivy/pyjnius/pull/522) allow ignoring private methods and fields in autoclass (both default to False)
    - (https://github.com/kivy/pyjnius/pull/503) auto detect java_home on OSX, using `/usr/libexec/java_home` (if JAVA_HOME is not declared)
    - (https://github.com/kivy/pyjnius/pull/514) writing to static fields (and fix reading from them)
    - (https://github.com/kivy/pyjnius/pull/517) make signature exceptions more useful
    - (https://github.com/kivy/pyjnius/pull/502) provide a stacktrace for where JVM was started.
    - (https://github.com/kivy/pyjnius/pull/523) expose the class's class attribute
    - (https://github.com/kivy/pyjnius/pull/524) fix handling of Java chars > 256 in Python3
    - (https://github.com/kivy/pyjnius/pull/519) Always show the exception name
    
    Fixed bugs:
    - (https://github.com/kivy/pyjnius/pull/481) wrong use of strip on JRE path
    - (https://github.com/kivy/pyjnius/pull/465) correct reflection to avoid missing any methods from parent classes or interfaces
    - (https://github.com/kivy/pyjnius/pull/508) don't had error details with a custom exception when java class is not found
    - (https://github.com/kivy/pyjnius/pull/510) add missing references to .pxi files in setup.py, speeding up recompilation
    - (https://github.com/kivy/pyjnius/pull/518) ensure autoclass prefers methods over properties
    - (https://github.com/kivy/pyjnius/pull/520) improved discovery of libjvm.so + provide a workaround if it doesn't work
    
    Documentation:
    - (https://github.com/kivy/pyjnius/pull/478) document automatic Thread detach feature
    - (https://github.com/kivy/pyjnius/pull/512) document the requirement to keep reference to object/functions passed to java, for as long as it might use them
    - (https://github.com/kivy/pyjnius/pull/521) fix inheritance in example
    
    Many thanks to the contributors that stepped up to help this release, it wouldn't have been possible without them.
    
    Craig Macdonald, Gabriel Pettier, Jim, André Miras, Young Ryul Bae, yyang, Pascal Chambon, Kevin Ollivier, Guillaume Gay, Christian M. Salamut, collaborated for this release.

--- jnius/__init__.py
+++ jnius/__init__.py
@@ -7,7 +7,7 @@ Accessing Java classes from Python.
 All the documentation is available at: http://pyjnius.readthedocs.org
 '''
 
-__version__ = '1.2.2-dev0'
+__version__ = '1.3.0-dev0'
 
 from .env import get_jnius_lib_location, get_jdk_home
 
