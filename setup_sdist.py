3b25c2c4abdcd23de617b0bf7c0cd2e465659161
Mirko Galimberti <me@mirkogalimberti.com>
2024-05-25 12:24:03 +0200

    Remove support for Python 3.7 as reached EOL (#713)

--- setup_sdist.py
+++ setup_sdist.py
@@ -56,7 +56,6 @@ SETUP_KWARGS = {
         'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX :: Linux',
         'Operating System :: Android',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
