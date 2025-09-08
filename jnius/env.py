ec53f965153ffe2d68555c005f37c4e0566d2fe6
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2025-09-08 21:59:03 +0100

    added jnius.env

---
+++ jnius/env.py
@@ -0,0 +1,3 @@
+from jnius_config.env import get_java_setup, JavaLocation, WindowsJavaLocation, BSDJavaLocation, AndroidJavaLocation
+from jnius_config.env import get_jre_home, get_jdk_home
+# this file is just a shim - env has been moved to jnius_config.env
